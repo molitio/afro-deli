431fb9b0453e25459911a87c5a309c0f8c6845aa
peterkollerlv <peterkollerlv@live.com>
2021-05-13 09:26:28 +0200

    initial commit

---
+++ afro-deli-ui/src/components/common/SiteLogoComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from '../../images/afroDeliLogo.jpg';
+export function SiteLogoComponent(): JSX.Element {
+    return (
+        <>
+            <img src={logo} alt="Logo" />
+        </>
+    );
+}
